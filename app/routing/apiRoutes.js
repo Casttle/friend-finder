1f300e41b4c4af1552f244e0ceb10cb00185ff6d
Casttle <cberry1222@gmail.com>
2018-10-06 14:08:09 -0500

    commit

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,7 +4,25 @@ module.exports = (app) => {
     app.get("/api/friends", (req, res) => {
         res.json(friendList);
       });
+
     app.post("/api/friends", (req, res) => {
         friendList.push(req.body);
-      });
+
+    let arrSum = friendList[0].scores.reduce((total, num) => {return total + num});
+        console.log(arrSum);
+    var userSum = friendList[2].scores;
+    //     userSum = userSum.forEach(item =>{
+    //     parseInt(item);
+    // });
+    console.log(userSum);    
+        // userArray = friendList.pop();
+
+        // friendList.forEach(item => {
+        //     parseInt(item.scores);
+        //     console.log(item.scores);
+        // });
+
+
+        
+      });   
 }
