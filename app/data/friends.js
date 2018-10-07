2a80942985aa31fe6f9925460d9acf1fc6346c25
Casttle <cberry1222@gmail.com>
2018-10-06 20:48:21 -0500

    commit

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,23 +1,23 @@
 const friendArray = [
     {
-        "name":"Ahmed",
+        "name":"Cartman",
         "photo":"https://pbs.twimg.com/profile_images/732220873021755393/b8ZTBHaL.jpg",
-        "scores":[5,1,4,4,5,1,2,5,4,1]
+        "scores":[5,1,4,4,5,1,2,5,4,5]
       },
       {
-        "name":"Bobby",
+        "name":"Wendy",
         "photo":"https://static.tvtropes.org/pmwiki/pub/images/wendy-testaburger_4251.jpg",
-        "scores":[2,1,5,4,5,1,2,5,4,1]
+        "scores":[2,1,5,4,5,1,2,5,3,4]
       },
       {
-        "name":"Bobby",
+        "name":"Drunk Randy",
         "photo":"https://orig00.deviantart.net/7b66/f/2017/283/3/c/sp___drunk_randy_by_fluffymystic-dbq72hj.png",
-        "scores":[2,1,5,4,5,1,2,5,4,1]
+        "scores":[2,3,5,4,1,5,2,1,4,5]
       },
       {
-        "name":"Bobby",
+        "name":"Butters",
         "photo":"https://upload.wikimedia.org/wikipedia/en/0/06/ButtersStotch.png",
-        "scores":[2,1,5,4,5,1,2,5,4,1]
+        "scores":[4,1,5,4,5,1,2,3,4,2]
       }
   ];
   
